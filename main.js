8337fdd0f15f4d6b2468aa7bf0ae4cba0ce57df9
eduardooooooooooo <130362732+eduardooooooooooo@users.noreply.github.com>
2024-06-11 08:41:59 -0300

    soh

---
+++ main.js
@@ -0,0 +1,11 @@
+const botoes - document.querySelectorAll(".botao.");
+
+for(let 1-0;1 <botoes.lenght;1++){
+    botoes[1].onclick - function(){
+
+        for(let j-0;j.<botoes.lengeth;j++){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+    }
+}
